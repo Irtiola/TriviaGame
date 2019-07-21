51f27241c79809730da5c93bfd2544deb3a00db7
Irina K <irtiola@gmail.com>
2019-07-21 12:26:07 -0700

    chaged css and added js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,11 +20,11 @@ var correctAnswers = 0;    //create var for counting correct questions
         $("h3").text(quizQ[counterQ].question);
         for (var i = 0; i < quizQ[counterQ].optionsOfAnswers.length; i++){
         $("div").append("<button>");
-        $("div").add("<p id ='time'>");
+        // $("div").add("<p id ='time'>");
         $("div button:last-child").text(quizQ[counterQ].optionsOfAnswers[i]);
         $("div button:last-child").attr("value", quizQ[counterQ].optionsOfAnswers[i]);
         $("div button:last-child").attr("disabled", false);
-        $("div").after("<p id='time'>")
+        $("div").prepend("<p id='time'>")
         }
     };
 
@@ -36,8 +36,8 @@ var correctAnswers = 0;    //create var for counting correct questions
             counterQ = 0;
             qTime = 10;
             $("div").empty();
-            $("div").append("<h1>");
-            $("h1").text("Javascript & Jquery quiz");
+            // $("div").append("<h1>");
+            // $("h1").text("Javascript & Jquery quiz");
             $("div").append("<h3>");
             showQuizCard();
             
@@ -47,18 +47,32 @@ var correctAnswers = 0;    //create var for counting correct questions
     function countBackwards(){
         qTime--;
         $("#time").text("Time:  " + qTime);
-        clearInterval(timeOut);
         if (qTime==0){
+            $("#time").after("<p id='result'>");
             $("#result").text("Time is out! Correct answer is: " + quizQ[counterQ].correctAnsw);
             unansweredQuestions+=1;
                 $("#result").after('<button type="next">');
-                $("button[type='next']").text("Next")
+                $("button[type='next']").text("Next");
+                clearInterval(timeOut);
+                
+                
         }
+        
 	}
- var timeOut = setInterval(countBackwards, 1*1000);
+    var timeOut = setInterval(countBackwards, 1*1000);
 
- function nextQuestion(){
-     $("<button type='primary'>");
-
-
- }
+    function guessTheAnswer(){
+        var userCh = $(this).val();
+        console.log(userCh);
+        if(userCh == quizQ[counterQ].correctAnsw){
+        $("#result").text("Nice job! Correct answer is: " + quizQ[counterQ].correctAnsw);
+        correctAnswers+=1;
+        }
+    }
+//  $("button[type='next']").on("click", function(){
+//     $("div").empty();
+//     $("div").append("<h1>");
+//     $("h1").text("Javascript & Jquery quiz");
+//     $("div").append("<h3>");
+//      showQuizCard();
+//  })
